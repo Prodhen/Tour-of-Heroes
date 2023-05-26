f23589404e6581432460c730d34b924b7d6225b6
Prodhen <mdaroshpodhen8@gmail.com>
2023-05-26 23:49:31 +0600

    lesson-4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { Houselocation } from '../houselocation';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-<section>
-<app-housing-location> </app-housing-location>
-</section>
+      <section>
+        <form>
+          <input type="text" placeholder="Filter by city">
+          <button class="primary" type="button">Search</button>
+        </form>
+      </section>
+    <section>
+    <app-housing-location> </app-housing-location>
+    </section>
     
 `,
     styleUrls: ['./home.component.css'],
     imports: [CommonModule, HousingLocationComponent]
 })
+
 export class HomeComponent {
+  housingLocation:Houselocation={
+    id:9999,
+    name:'Test Home',
+    city:'test City',
+    state:'ST',
+    photo:'assets/example-house.jpj',
+    availableUnits:99,
+    wifi:true,
+    laundry:false
+  };
 
 }
