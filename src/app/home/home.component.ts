e31514580c75327c31d948a5cd5713ae3f035a07
Prodhen <mdaroshpodhen8@gmail.com>
2023-05-27 23:23:39 +0600

    lesson-8

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,9 @@ import { Houselocation } from '../houselocation';
         </form>
       </section>
     <section>
-    <app-housing-location [houseLocation]="houseLocation"> </app-housing-location>
+    <app-housing-location *ngFor="let houseLocation of housingLocationList"
+    
+    [houseLocation]="houseLocation"> </app-housing-location>
     </section>
     
 `,
@@ -23,15 +25,107 @@ import { Houselocation } from '../houselocation';
 })
 
 export class HomeComponent {
-  houseLocation:Houselocation={
-    id:9999,
-    name:'Test Home',
-    city:'test City',
-    state:'ST',
-    photo:'assets/example-house.jpj',
-    availableUnits:99,
-    wifi:true,
-    laundry:false
-  };
+  housingLocationList: Houselocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/home1.JPG',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '/assets/home2.JPG',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '/assets/home3.JPG',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/home4.JPG',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: '/assets/home5.JPG',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/home6.JPG',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/home7.JPG',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/home8.JPG',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/home1.JPG',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: '/assets/home5.JPG',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true
+    }
+  ];
 
 }
